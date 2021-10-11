d0e2af7d854569d159eae5af5d1e8f3786b06466
axpedro <pedruax@hotmail.com>
2021-10-11 17:57:13 -0300

    nova página

--- lib/controllers/routes.dart
+++ lib/controllers/routes.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:teacher_helper/Pages/calendario.dart';
 import 'package:teacher_helper/Pages/configuracao/configuracao.dart';
 import 'package:teacher_helper/Pages/home/home.dart';
 
 var routes = <String, WidgetBuilder>{
   '/': (context) => HomePage(),
   '/configuracoes': (context) => const ConfiguracaoPage(),
+  '/calendario': (context) => const CalendarioPage(),
+
 };
