2dccc5f7da56532d7b31ab222e0b560f4e49b0b4
danielfsc <girardi1309@gmail.com>
2021-10-13 09:30:15 -0300

    Fix: import do sfcalendar que deletei errado.

--- lib/Pages/calendario/calendario.dart
+++ lib/Pages/calendario/calendario.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 import 'package:teacher_helper/Pages/calendario/eventos.dart';
 import 'package:teacher_helper/shared/page_mask.dart';
-import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 class CalendarioPage extends StatelessWidget {
   const CalendarioPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return PageMask( 
+    return PageMask(
       title: 'Calendário',
-        body: SfCalendar(
-        view:CalendarView.month,
+      body: SfCalendar(
+        view: CalendarView.month,
         showNavigationArrow: true,
         firstDayOfWeek: 1,
         initialDisplayDate: DateTime.now(),
@@ -20,23 +20,16 @@ class CalendarioPage extends StatelessWidget {
         //backgroundColor: Color.fromRGBO(0, 0, 0, 0),
         todayHighlightColor: Colors.red,
         dataSource: MeetingDataSource(getAppointments()),
-        ),
-
-        floatingButton: FloatingActionButton (onPressed: (){
+      ),
+      floatingButton: FloatingActionButton(
+        onPressed: () {
           print('teste');
-                  },
-                  child: const Icon(Icons.plus_one),
+        },
+        child: const Icon(Icons.plus_one),
         backgroundColor: Colors.red,
-        
-        
-        
-        ),
+      ),
     );
-    
-      
-   
-    }
+  }
 }
 
-class EventEditingPage {
-}
+class EventEditingPage {}
