4be5ef2da5a97b28ada5d0835f62c493c9855c93
axpedro <67110951+axpedro@users.noreply.github.com>
2021-10-13 09:18:18 -0300

    Calendario+eventos (#1)
    * trocando titulo da home para teste
    
    * nova página
    
    * calendario com eventos
    
    * floatingbutton
    
    * Update home.dart
    
    Co-authored-by: danielfsc <girardi1309@gmail.com>

---
+++ lib/Pages/calendario/calendario.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:teacher_helper/Pages/calendario/eventos.dart';
+import 'package:teacher_helper/shared/page_mask.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+class CalendarioPage extends StatelessWidget {
+  const CalendarioPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return PageMask( 
+      title: 'Calendário',
+        body: SfCalendar(
+        view:CalendarView.month,
+        showNavigationArrow: true,
+        firstDayOfWeek: 1,
+        initialDisplayDate: DateTime.now(),
+        initialSelectedDate: DateTime.now(),
+        cellBorderColor: Colors.red,
+        //backgroundColor: Color.fromRGBO(0, 0, 0, 0),
+        todayHighlightColor: Colors.red,
+        dataSource: MeetingDataSource(getAppointments()),
+        ),
+
+        floatingButton: FloatingActionButton (onPressed: (){
+          print('teste');
+                  },
+                  child: const Icon(Icons.plus_one),
+        backgroundColor: Colors.red,
+        
+        
+        
+        ),
+    );
+    
+      
+   
+    }
+}
+
+class EventEditingPage {
+}
