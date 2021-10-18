35ecc201ea970e0741e5fbf02aeaa5d22fa299dc
Kazeshini3 <tomecosabs@gmail.com>
2021-10-18 20:04:29 -0300

    Nova versao

--- lib/controllers/routes.dart
+++ lib/controllers/routes.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:teacher_helper/Pages/calendario/calendario.dart';
+//import 'package:teacher_helper/Pages/calendario/calendario.dart';
 import 'package:teacher_helper/Pages/configuracao/configuracao.dart';
+import 'package:teacher_helper/Pages/dynamic_event.dart';
 import 'package:teacher_helper/Pages/home/home.dart';
 
 var routes = <String, WidgetBuilder>{
   '/': (context) => HomePage(),
   '/configuracoes': (context) => const ConfiguracaoPage(),
-  '/calendario': (context) => const CalendarioPage(),
+  //'/calendario': (context) => const CalendarioPage(),
+  '/calendario': (context) => const DynamicEvent(),
 };
